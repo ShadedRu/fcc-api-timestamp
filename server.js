0202cdb1d8d85e49d5a616824eed737dc825a851
Alexander <shadedvologda@gmail.com>
2017-04-10 15:48:20 +0000

    done

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.get('/*', function (req, res) {
   var query = req.url.toString().slice(1);
   var isNum = /^\d+$/.test(query);
   if (isNum) {
-    res.end(JSON.stringify(format_natural(query*1000)));
+    res.end(JSON.stringify(format(query*1000)));
   } else {
     var date = new Date(query.replace(/%20/g, ' '));
     if (date != 'Invalid Date') {
-      res.end(JSON.stringify(format_natural(date.getTime())));
+      res.end(JSON.stringify(format(date.getTime())));
     } else {
       res.end(JSON.stringify({unix:null, natural:null}));
     }
@@ -26,7 +26,7 @@ app.listen(port, function () {
   console.log('Example app listening on port ' + port + "!");
 });
 
-function format_natural (input) {
+function format (input) {
   var date = new Date(parseInt(input));
   var monthName = ['Janury', 'February', 'March', 'April',
                   'May', 'June', 'July', 'August',
