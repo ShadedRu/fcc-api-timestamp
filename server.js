e89a55eac014dc48a143cebaa60a5e1de85c8162
Alexander <shadedvologda@gmail.com>
2017-04-10 15:46:37 +0000

    done

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 var express = require('express');
+var url = require('url');
 var app = express();
-var port = 8080
+var port = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/*', function (req, res) {
+  var query = req.url.toString().slice(1);
+  var isNum = /^\d+$/.test(query);
+  if (isNum) {
+    res.end(JSON.stringify(format_natural(query*1000)));
+  } else {
+    var date = new Date(query.replace(/%20/g, ' '));
+    if (date != 'Invalid Date') {
+      res.end(JSON.stringify(format_natural(date.getTime())));
+    } else {
+      res.end(JSON.stringify({unix:null, natural:null}));
+    }
+  }
 });
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + "!");
 });
+
+function format_natural (input) {
+  var date = new Date(parseInt(input));
+  var monthName = ['Janury', 'February', 'March', 'April',
+                  'May', 'June', 'July', 'August',
+                  'September', 'October', 'November', 'December'];
+
+  var unix_time = Math.floor(date.getTime() / 1000);
+  var natural = monthName[date.getMonth()].toString() +
+                ' ' + date.getDate().toString() +
+                ', ' + date.getFullYear().toString();
+  
+  return {unix: unix_time, natural: natural};
+}
